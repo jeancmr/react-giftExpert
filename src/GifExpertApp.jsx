843634c8c7088139064a699cb14ece4269c5346f
jeancmr <jean.madiedo08@gmail.com>
2024-06-03 19:30:59 -0500

    First commit

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react"
+import { AddCategory, GifGrid } from "./components"
+
+export const GifExpertApp = () => {
+    const [categories, setCategories] = useState(['Black Clover'])
+
+    const onAddCategory = (newCategory) => {
+        const lowerCaseCategories = categories.map(category => category.toLowerCase())
+        const lowerCaseNewCategory = newCategory.toLowerCase()
+        if (lowerCaseCategories.includes(lowerCaseNewCategory)) return
+
+        setCategories([newCategory, ...categories])
+    }
+
+    return (
+        <>
+            <h1>GifExpertApp</h1>
+
+            <AddCategory
+                // setCategories={setCategories} 
+                onNewCategory={(event) => onAddCategory(event)}
+            />
+
+            {categories.map((category) =>
+                <GifGrid
+                    key={category}
+                    category={category}
+                />
+            )
+            }
+
+        </>
+    )
+}
